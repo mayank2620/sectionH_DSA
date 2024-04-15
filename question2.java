942db432cc308ed16364b2f0fe2f6603a4610753
Mayank Sharma <114276128+mayank2620@users.noreply.github.com>
2024-04-15 15:18:34 +0530

    Update question2.java

--- question2.java
+++ question2.java
@@ -1,7 +1,7 @@
 //Question 2
 import java.util.Scanner;
 
-public class Main {
+public class question2 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
